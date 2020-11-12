4f869e01056affe93093b32b907a48de7a81daa4
Nikolai Vasiliev <63501861+krankenmorder@users.noreply.github.com>
2020-11-13 00:17:49 +0300

    version 1.1
    Создание класса Battery и трёх его классов-наследников Li_Ion, Ni_Cd и Ni_MH. Создание конструкторов и деструкторов во всех классах-наследниках и классе-предке.

--- LabOOP_3/LabOOP_3.cpp
+++ LabOOP_3/LabOOP_3.cpp
@@ -1,5 +1,73 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+class Battery {
+public:
+    string name;
+    int capacity;
+    Battery() {
+        name = " ";
+        capacity = 0;
+    }
+    ~Battery() {
+        printf("Объект удалён\n\n");
+    }
+};
+
+class Li_Ion : public Battery {
+public:
+    Li_Ion() { 
+        name = "Li-Ion";
+        capacity = 0;
+    }
+    Li_Ion(int capacity) {
+        this->capacity = capacity;
+    }
+    Li_Ion(Li_Ion& i) {
+        capacity = i.capacity;
+    }
+    ~Li_Ion() {
+
+    }
+};
+
+class Ni_Cd : public Battery {
+public:
+    Ni_Cd() {
+        name = "Ni-Cd";
+        capacity = 0;
+    }
+    Ni_Cd(int capacity) {
+        this->capacity = capacity;
+    }
+    Ni_Cd(Ni_Cd &i) {
+        capacity = i.capacity;
+    }
+    ~Ni_Cd() {
+
+    }
+};
+
+class Ni_MH : public Battery {
+public:
+    Ni_MH() {
+        name = "Ni-MH";
+        capacity = 0;
+    }
+    Ni_MH(int capacity) {
+        this->capacity = capacity;
+    }
+    Ni_MH(Ni_MH &i) {
+        capacity = i.capacity;
+    }
+    ~Ni_MH() {
+
+    }
+};
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
